f46d3b306840fc8c29f1de5cc1dbdbac0cf6730f
Kieran Loewen <kieranloewen@live.com>
2016-06-11 23:34:17 -0500

    Updated copy step of script to check filesize

--- CopySpotlight.ps1
+++ CopySpotlight.ps1
@@ -5,7 +5,6 @@ param ([string]$OutputPath = $env:USERPROFILE + "\Pictures\Spotlight")
 if(!(Test-Path -Path $OutputPath )){
 	Write-Host "Output folder does not exist, creating it"
     md $OutputPath
-}else {
 }
 
 if(!$OutputPath.EndsWith("\")) {
@@ -14,6 +13,6 @@ if(!$OutputPath.EndsWith("\")) {
 
 Write-Host "Copying files to" $OutputPath
 
-Get-ChildItem -Path $env:LOCALAppData\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets | Copy-Item -dest {$OutputPath + $_.BaseName + ".jpg" }
+Get-ChildItem  -Path $env:LOCALAppData\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets | where {$_.Length -gt 50KB} | Copy-Item -dest {$OutputPath + $_.BaseName + ".jpg" }
 
 Write-Host "File copy complete"
